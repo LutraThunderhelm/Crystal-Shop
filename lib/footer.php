2e68a1069af2cc81f6fc0391a7295d370c8f53e5
Tyler Odom <todom8@bcst01.parkland.edu>
2022-03-21 08:30:54 -0500

    Added basic files

---
+++ lib/footer.php
@@ -0,0 +1,15 @@
+<?php
+echo'
+	<ul>
+		<li><a href="/Crystal-Shop/login.php">Login</a></li>
+                <li><a href="/Crystal-Shop/cart.php">Cart</a></li>
+	</ul>
+	<ul>
+		Magical Crystals
+		<li><a href="Crystal-Shop/products/ignus.php">Ignus</a></li>
+                <li><a href="Crystal-Shop/products/frigus.php">Frigus</a></li>
+                <li><a href="Crystal-Shop/products/lux.php">Lux</a></li>
+                <li><a href="Crystal-Shop/products/industria.php">Industria</a></li>
+	</ul>
+';
+?>
---
+++ lib/header.php
@@ -0,0 +1,4 @@
+<?php echo '<!-- I honor Parkland's core values by affirming that I have 
+followed all academic integrity guidelines for this work. Tyler Odom,
+CSC-155 -->';
+?>
